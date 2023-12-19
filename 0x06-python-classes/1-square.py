381f4f9c6e715cadfdf6305afa881c117035aaf9
M-A-dibshik <dibshik55@gamil.com>
2023-12-19 19:12:27 +0200

    f 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""I'm learn"""
+
+
+class Square:
+    """Define"""
+
+    def __int__(self, size):
+        """constr
+
+        Args:
+        size: length of a side of the square.
+        """
+        self.__size = size
