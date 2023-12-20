091ef62598e2c3632e3fa2d58ca125e69553d09a
M-A-dibshik <dibshik55@gamil.com>
2023-12-20 19:05:51 +0200

    f 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Square"""
+
+
+class Square:
+    """Define"""
+
+    def __init__(self, size=0):
+        """Constr
+
+
+        Args:
+                size: length os side of square
+
+        Raises:
+                TypeError: if size not integer
+                ValueError: if size less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            """Area of this square
+
+            Return:
+                    size square
+
+            """
+            return self.__size**2
