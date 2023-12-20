24d7aae1be69a8a9d2a9ce01de4a7c4f6ff9aded
M-A-dibshik <dibshik55@gamil.com>
2023-12-20 18:38:50 +0200

    f 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,20 +3,20 @@
 
 
 class Square:
-	""" Define"""
+    """Define"""
 
-	def __init__(self, size=0):
-		"""Constr.
+    def __init__(self, size=0):
+        """Constr.
 
-		Args:
-			size: length of side of square.
+        Args:
+                size: length of side of square.
 
-		Raises:
-			TypeError: if size is not an integer
-			ValueError: if size is less than 0
-		"""
-		if not isinstance(size, int):
-			raise TypeError('size must be an integer')
-		if size < 0:
-			raise ValueError('size must be >= 0')
-		self.__size = size
+        Raises:
+                TypeError: if size is not an integer
+                ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
