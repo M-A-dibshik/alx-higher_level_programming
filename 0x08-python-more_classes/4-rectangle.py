a7c2279a93ed82871470237112b4d9de6ffd4ad0
M-A-dibshik <dibshik55@gamil.com>
2024-01-05 20:00:00 +0200

    f 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -60,4 +60,4 @@ class Rectangle:
 
     def __repr__(self):
         """return """
-        return "Rectangle({:d}, {:d}".format(self.__width, self.__height)
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
