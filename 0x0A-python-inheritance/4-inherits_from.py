f5eb633b66eb4b487d7ed3aad2272bc8a31718f9
M-A-dibshik <dibshik55@gamil.com>
2024-01-09 12:19:53 +0200

    f 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
-#!usr/bin/python3
-'''Module inherits'''
+#!/usr/bin/python3
+'''Module for inherits_from method.'''
 
 
 def inherits_from(obj, a_class):
-    '''object is an instance of a class that inherited'''
+    '''Determines if an object is a true subclass of a class.'''
     return isinstance(obj, a_class) and type(obj) != a_class
