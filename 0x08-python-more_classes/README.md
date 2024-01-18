622cbd9e00093986499c5aeea326aba162183111
M-A-dibshik <dibshik55@gamil.com>
2024-01-18 16:34:41 +0200

    delet task

--- 0x08-python-more_classes/README.md
+++
@@ -1,1 +0,0 @@
-readme file python-more_classes
--- 0x0A-python-inheritance/2-is_same_class.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Module"""
-
-
-def is_same_class(obj, a_class):
-    """object instance class"""
-
-    return type(obj) == a_class
